e1807976d8d2e9276025797ac266893dea1a0981
mickmed <mick2090@gmail.com>
2020-10-11 18:45:19 -0400

    gitignore

--- client/src/Components/apiConfig.js
+++ client/src/Components/apiConfig.js
@@ -4,7 +4,7 @@ let apiUrl
 
 const apiUrls = {
     production: 'https://mix-trail.herokuapp.com/api',
-    development: 'http://localhost:3000/api'
+    development: 'https://mix-trail.herokuapp.com/api'
 }
 
 if (window.location.hostname === 'localhost') {
