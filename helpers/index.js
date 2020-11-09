b28345212377cc0b7f61f5cc70ea4dd44351d97e
mickmed <mick2090@gmail.com>
2020-11-09 00:29:32 -0500

    debug jwt on deployed

--- helpers/index.js
+++ helpers/index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
   const jwt = require("jsonwebtoken")
   const TOKEN_KEY = process.env.SECRET_KEY
 
+  console.log('sssssssssssssssssssssssssssssssssssssssssss', TOKEN_KEY)
+
   const token = req.headers.authorization.split(" ")[1]
   const data = jwt.verify(token, TOKEN_KEY)
 
