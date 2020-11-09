a8c039d73f45bed84206814775de6565c65b7d72
mickmed <mick2090@gmail.com>
2020-11-09 00:37:36 -0500

    auth debug

--- helpers/index.js
+++ helpers/index.js
@@ -8,6 +8,8 @@ require('dotenv').config()
   console.log('sssssssssssssssssssssssssssssssssssssssssss', TOKEN_KEY)
 
   const token = req.headers.authorization.split(" ")[1]
+
+  console.log('tttttttttttttttttttttttttttttt', token)
   const data = jwt.verify(token, TOKEN_KEY)
 
   try {
