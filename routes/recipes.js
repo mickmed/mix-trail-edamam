df74cef613d3adbc6f18194ded856411655d5fd8
mickmed <mick2090@gmail.com>
2021-02-27 23:14:21 -0500

    recipe list

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,23 +1,19 @@
-const { Router } = require("express")
-const controllers = require("../controllers/recipes")
-const authControllers = require('../controllers/auth')
-const restrict = require('../helpers')
+const { Router } = require("express");
+const controllers = require("../controllers/recipes");
+const authControllers = require("../controllers/auth");
+const restrict = require("../helpers");
 
+const router = Router();
 
+router.get("/", controllers.getRecipes);
+router.get("/:id", controllers.getRecipe);
+router.post("/", restrict, controllers.createRecipe);
+router.put("/:id", controllers.updateRecipe);
 
-const router = Router()
+router.delete("/:id", restrict, controllers.deleteRecipe);
 
-router.get("/", controllers.getRecipes)
-router.get("/:id", controllers.getRecipe)
-router.post("/", restrict, controllers.createRecipe)
-router.put("/:id", controllers.updateRecipe)
-
-router.delete("/:id", restrict, controllers.deleteRecipe)
-
-
-router.get('/users/:id', controllers.getRecipesByUser)
+router.get("/users/:id", controllers.getRecipesByUser);
 
 // router.get('/fix', controllers.fixNutrientVals)
 
-
-module.exports = router
+module.exports = router;
